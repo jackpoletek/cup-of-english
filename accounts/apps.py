92752836be510186c0b3c9022682105f876724d3
jackpoletek <arrayatbay@protonmail.com>
2026-02-02 14:13:14 +0000

    Configure local environment and create profile model

--- accounts/apps.py
+++ accounts/apps.py
@@ -3,3 +3,10 @@ from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+def ready(self):
+        """
+        This method is called when Django starts. It imports the signals module
+        to ensure that signal handlers are connected.
+        """
+        import accounts.signals
