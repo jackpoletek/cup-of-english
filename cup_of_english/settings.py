50c8480f4640ef90de22996a3228e9eb48afd666
jackpoletek <arrayatbay@protonmail.com>
2026-03-26 06:02:41 +0000

    Configure Gmail for ecommerce with app password for email sending

--- cup_of_english/settings.py
+++ cup_of_english/settings.py
@@ -144,3 +144,11 @@ LOGGING = {
 STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 STRIPE_WEBHOOK_SECRET = os.environ.get("STRIPE_WEBHOOK_SECRET")
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
