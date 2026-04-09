a5dad3bca8fb6fde0a0c5704d3f5b85a79c29966
jackpoletek <arrayatbay@protonmail.com>
2026-04-09 15:51:33 +0100

    Add form for updating user email

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,6 +6,13 @@ from django.contrib.auth.forms import UserCreationForm
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
-class Meta:
-    model = User
-    fields = ("username", "email", "password1", "password2")
+    # Meta class to specify the model and fields to use
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+# CRUD operations on profile
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["email"]
