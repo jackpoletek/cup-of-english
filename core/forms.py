a7fbd03755c0b0e5801511de2737cc48a9d929a0
jackpoletek <arrayatbay@protonmail.com>
2026-05-18 14:09:52 +0100

    Add captcha to contact form

--- core/forms.py
+++ core/forms.py
@@ -3,6 +3,24 @@ from .models import ContactMessage
 
 
 class ContactForm(forms.ModelForm):
+
+    # Simple CAPTCHA field to prevent spam
+    captcha = forms.IntegerField(
+        label="What is 3 + 4?",
+        required=True,
+        min_value=0
+    )
+
     class Meta:
         model = ContactMessage
         fields = ['name', 'email', 'message']
+
+    def clean_captcha(self):
+        captcha_answer = self.cleaned_data.get('captcha')
+
+        if captcha_answer != 7:
+            raise forms.ValidationError(
+                "Incorrect answer to the CAPTCHA question."
+                )
+
+        return captcha_answer
--- teachers/views.py
+++ teachers/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 from accounts.models import UserProfile
 
+
 def teacher_list(request):
 
     teachers = UserProfile.objects.filter(role="teacher").select_related("user")
