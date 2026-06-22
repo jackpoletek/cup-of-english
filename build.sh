6b204b4022397d94b71e9d7ed8c5d39a4e900738
jackpoletek <arrayatbay@protonmail.com>
2026-06-22 13:30:12 +0100

    Add debug findstatic command to build.sh for troubleshooting

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 set -o errexit
 
-static/cup_of_english/images/about.jpg
-
+python manage.py findstatic cup_of_english/images/about.jpg --verbosity 2
 python manage.py collectstatic --noinput
 python manage.py migrate
